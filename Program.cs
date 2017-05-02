4389f9a203aebe060ae8a2980cf54882b0167554
tonybamboo <tonybamboo@gmail.com>
2017-05-02 11:12:33 +0100

    Centered Education

--- Program.cs
+++ Program.cs
@@ -1,32 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using System.IO;
-
 using System.Linq;
 using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Hosting;
 
-
-
 namespace dotnet
-
 {
+
 public class Program
-
 {
-public static void Main(string[] args)
 
+public static void Main(string[] args)
 {
 var host = new WebHostBuilder().UseKestrel()
-			       .UseContentRoot(Directory.GetCurrentDirectory())
-			       .UseIISIntegration()
-			       .UseStartup<Startup>()
-                   .Build();
-
+						       .UseContentRoot(Directory.GetCurrentDirectory())
+						       .UseIISIntegration()
+			      			   .UseStartup<Startup>()
+                   			   .Build();
 host.Run();
-
 }  
 }
 }
